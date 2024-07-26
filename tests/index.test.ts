41441e2836ec6af7083fd4d3e8fac1b6f5dd0f37
Michael James <micrjames921@gmail.com>
2024-07-26 00:17:40 -0700

    completed tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,8 @@
 import { YANode } from "../YANode";
 
 describe("A node", () => {
-      let node1: YANode<number>;
-      let node2: YANode<number>;
+   let node1: YANode<number>;
+   let node2: YANode<number>;
    beforeAll(() => {
 	  node2 = {
 		 Key: 2,
@@ -14,9 +14,17 @@ describe("A node", () => {
 	  };
    });
    describe("Given information", () => {
-	  test.todo("Should have a correctly typed key.");
-	  test.todo("Should point next to a node.");
-	  test.todo("Should hold the value in its key.");
-	  test.todo("Should point to the next node.");
+	  test("Should have a correctly typed key.", () => {
+		  expect(typeof node1.Key).toBe("number");
+	  });
+	  test("Should point next to a node.", () => {
+		  expect(node1.next).not.toBeNull();
+	  });
+	  test("Should hold the value in its key.", () => {
+		  expect(node1.Key).toBe(1);
+	  });
+	  test("Should point to the next node.", () => {
+		  expect(node1.next).toEqual(node2);
+	  });
    });
 });
